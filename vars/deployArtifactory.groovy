3197aad1af0c6c3635599a2afa0fe6981ebef017
costich88 <const_nov@outlook.com>
2019-04-22 13:34:15 +0300

    added libs

--- vars/deployArtifactory.groovy
+++ vars/deployArtifactory.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String username, String password,) {
+def call(String username, String password) {
      sh script: "curl -u ${username}:${password} -T artifacts.zip http://artifactory:8081/artifactory/generic-local/artifacts.zip", returnStdout: true
 }
